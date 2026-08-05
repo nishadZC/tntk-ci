b9dc02a6f4f556ba73cec79d483e3555c9717ed6
nishadzc <vinayak.nishad@zapcg.com>
2026-08-05 12:32:40 +0530

    feat: implement GORM database connection management and automatic user table migration

--- src/api/api/dto.go
+++ src/api/api/dto.go
@@ -26,6 +26,6 @@ func getGormDb() *gorm.DB{
 
 func getDatabaseConnectionInfo() string {
   return fmt.Sprintf("host=%s port=%s user=%s "+
-    "password=%s dbname=%s sslmode=disable",
+    "password=%s dbname=%s sslmode=require",
     getDbHost(), getDbPort(), getDbUsername(), getDbPassword(), getDbName())
 }
